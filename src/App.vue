d3c0fa2bba108034e02a7b0632198cff49636dc9
zrj <rj527667654@163.com>
2023-03-29 18:00:41 +0800

    cesium初体验

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+  
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ src/views/Cesium.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="cesiumContainer">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import * as Cesium from 'cesium'
+import { onMounted } from 'vue'
+onMounted(() => {
+    const viewer = new Cesium.Viewer('cesiumContainer')
+})
+</script>
+
+<style lang="less">
+#cesiumContainer {
+  width: 90%;
+  height: 90%;
+  margin: auto;
+}
+</style>
